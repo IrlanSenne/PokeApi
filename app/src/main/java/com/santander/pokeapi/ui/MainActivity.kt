618011be3fb7927016b66f222332fe5fe8140d44
Irlan Senne <im.senne2017@gmail.com>
2022-06-09 15:44:42 +0100

    Set stateFlow in viewModel

--- app/src/main/java/com/santander/pokeapi/ui/MainActivity.kt
+++ app/src/main/java/com/santander/pokeapi/ui/MainActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.lifecycleScope
 import com.santander.pokeapi.R
 import com.santander.pokeapi.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -20,12 +23,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-
-        viewModel.test.observe(this, {
-            binding.test.setText("${it.toString()}")
-        })
-
+        lifecycleScope.launchWhenCreated {
+            viewModel.pokemonsStateFlow.collectLatest {
+                binding.test.text = "$it"
+            }
+        }
     }
 }
--- app/src/main/java/com/santander/pokeapi/ui/PokemonsViewModel.kt
+++ app/src/main/java/com/santander/pokeapi/ui/PokemonsViewModel.kt
@@ -1,15 +1,13 @@
 package com.santander.pokeapi.ui
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.santander.pokeapi.common.Resource
 import com.santander.pokeapi.data.remote.Pokemons
 import com.santander.pokeapi.domain.use_case.get_pokemons.GetPokemonsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
@@ -17,10 +15,10 @@ import javax.inject.Inject
 @HiltViewModel
 class PokemonsViewModel @Inject constructor(
     private val getPokemonsUseCase: GetPokemonsUseCase
-): ViewModel() {
-//
-    private val _test = MutableLiveData<List<Pokemons>>()
-    var test : LiveData<List<Pokemons>> = _test
+) : ViewModel() {
+
+    private val _pokemonsStateFlow = MutableStateFlow<List<Pokemons>?>(null)
+    val pokemonsStateFlow = _pokemonsStateFlow.asStateFlow()
 
     init {
         getPokemons()
@@ -28,24 +26,19 @@ class PokemonsViewModel @Inject constructor(
 
     private fun getPokemons() {
         getPokemonsUseCase.invoke().onEach { result ->
-            when(result) {
+            when (result) {
                 is Resource.Success -> {
-                    //_state.value = PokemonsListState(pokemons = result.data?.pokemons ?: listOf())
-                    var a = result
-                    var b = result.data
-                    _test.postValue(result.data?.pokemons)
+                    _pokemonsStateFlow.value = result.data?.pokemons
                 }
                 is Resource.Error -> {
-                   // _state.value = PokemonsListState(error = "")
-                    _test.postValue(listOf())
+                    //TODO
+                    result.message
                 }
                 is Resource.Loading -> {
-                   // _state.value = PokemonsListState(isLoading = true)
-                    _test.postValue(listOf())
+                    //TODO
                 }
             }
 
         }.launchIn(viewModelScope)
-
     }
 }
