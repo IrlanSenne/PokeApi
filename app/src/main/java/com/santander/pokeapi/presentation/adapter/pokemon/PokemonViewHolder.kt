24a09641a1449bbd431745ed67647c6b330e68aa
Irlan Senne <im.senne2017@gmail.com>
2022-06-16 11:46:27 +0100

    Set attributes for custom view

--- app/src/main/java/com/santander/pokeapi/presentation/adapter/pokemon/PokemonViewHolder.kt
+++ app/src/main/java/com/santander/pokeapi/presentation/adapter/pokemon/PokemonViewHolder.kt
@@ -6,15 +6,11 @@ import com.bumptech.glide.Glide
 import com.santander.pokeapi.R
 import com.santander.pokeapi.presentation.adapter.BaseViewHolder
 import com.santander.pokeapi.presentation.ui.model.PokemonUI
+import com.santander.pokeapi.presentation.ui.widget.PokeContentCard
 
 class PokemonViewHolder(viewGroup: ViewGroup) : BaseViewHolder<PokemonUI?>(R.layout.item_pokemon, viewGroup) {
     override fun bind(item: PokemonUI?) {
-        itemView.findViewById<TextView>(R.id.tvPokemon).text = item?.name
-
-        Glide
-            .with(itemView)
-            .load(item?.imageUrl)
-            .centerCrop()
-            .into(itemView.findViewById(R.id.ivPokemon))
+        itemView.findViewById<PokeContentCard>(R.id.contentCard).setName(item?.name ?: "")
+        itemView.findViewById<PokeContentCard>(R.id.contentCard).setImage(item?.imageUrl ?: "")
     }
 }
---
+++ app/src/main/java/com/santander/pokeapi/presentation/ui/widget/PokeContentCard.kt
@@ -0,0 +1,35 @@
+package com.santander.pokeapi.presentation.ui.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
+import com.santander.pokeapi.R
+import com.santander.pokeapi.databinding.ContentCardBinding
+
+class PokeContentCard(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+
+    private val binding: ContentCardBinding = ContentCardBinding.inflate(
+        LayoutInflater.from(context), this, true
+    )
+
+    init {
+        context.obtainStyledAttributes(attrs, R.styleable.PokeContentCard).apply {
+            getString(R.styleable.PokeContentCard_text)?.let { setName(it) }
+            getString(R.styleable.PokeContentCard_src)?.let { setImage(it) }
+        }
+    }
+
+    fun setName(name: String) {
+        binding.tvPokemon.text = name
+    }
+
+    fun setImage(src: String) {
+        Glide
+            .with(binding.root)
+            .load(src)
+            .centerCrop()
+            .into(binding.ivPokemon)
+    }
+}
