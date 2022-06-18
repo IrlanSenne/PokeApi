4dd5ab8aeeb81bdc2537dfab81f27d0c97fbd798
Irlan Senne <im.senne2017@gmail.com>
2022-06-18 10:39:38 +0100

    Get details from service

--- app/src/main/java/com/santander/pokeapi/presentation/ui/PokemonsViewModel.kt
+++ app/src/main/java/com/santander/pokeapi/presentation/ui/PokemonsViewModel.kt
@@ -3,7 +3,9 @@ package com.santander.pokeapi.presentation.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.santander.pokeapi.common.Resource
+import com.santander.pokeapi.data.remote.Pokemon
 import com.santander.pokeapi.data.remote.Pokemons
+import com.santander.pokeapi.domain.use_case.get_detail_pokemon.GetPokemonDetailUseCase
 import com.santander.pokeapi.domain.use_case.get_pokemons.GetPokemonsUseCase
 import com.santander.pokeapi.presentation.ui.pokemon_main.mapper.toUI
 import com.santander.pokeapi.presentation.ui.model.PokemonUI
@@ -16,11 +18,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class PokemonsViewModel @Inject constructor(
-    private val getPokemonsUseCase: GetPokemonsUseCase
+    private val getPokemonsUseCase: GetPokemonsUseCase,
+    private val getPokemonDetailUseCase: GetPokemonDetailUseCase,
 ) : ViewModel() {
 
     private val _pokemonsStateFlow = MutableStateFlow<List<PokemonUI>?>(null)
     val pokemonsStateFlow = _pokemonsStateFlow.asStateFlow()
+
+    private val _pokemonDetailStateFlow = MutableStateFlow<Pokemon?>(null)
+    val pokemonDetailStateFlow = _pokemonDetailStateFlow.asStateFlow()
 
     init {
         getPokemons(350)
@@ -31,6 +37,17 @@ class PokemonsViewModel @Inject constructor(
             when (result) {
                 is Resource.Success -> {
                     _pokemonsStateFlow.value = result.data?.pokemons?.map { it.toUI() }
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    fun getPokemonDetail(name: String) {
+        getPokemonDetailUseCase.invoke(name).onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _pokemonDetailStateFlow.value = result.data
+
                 }
             }
         }.launchIn(viewModelScope)
--- app/src/main/java/com/santander/pokeapi/presentation/ui/pokemon_details/PokemonDetailFragment.kt
+++ app/src/main/java/com/santander/pokeapi/presentation/ui/pokemon_details/PokemonDetailFragment.kt
@@ -1,27 +1,41 @@
 package com.santander.pokeapi.presentation.ui.pokemon_details
 
+import androidx.compose.ui.text.toLowerCase
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.santander.pokeapi.databinding.PokemonDetailsBinding
 import com.santander.pokeapi.presentation.BaseFragment
 import com.santander.pokeapi.presentation.ui.MainActivity
 import com.santander.pokeapi.presentation.ui.PokemonsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import java.util.*
 
 @AndroidEntryPoint
-class PokemonDetailFragment: BaseFragment<PokemonDetailsBinding, PokemonsViewModel>() {
+class PokemonDetailFragment : BaseFragment<PokemonDetailsBinding, PokemonsViewModel>() {
 
     companion object {
         fun newInstance() = PokemonDetailFragment()
     }
 
-    override fun getViewBinding(): PokemonDetailsBinding = PokemonDetailsBinding.inflate(layoutInflater)
+    override fun getViewModel(): PokemonsViewModel =
+        ViewModelProvider(this)[PokemonsViewModel::class.java]
+
+    override fun getViewBinding(): PokemonDetailsBinding =
+        PokemonDetailsBinding.inflate(layoutInflater)
 
     override fun initializeUi() {
         val bundle = arguments
         val name = bundle?.getString(MainActivity.POKEMON_NAME)
 
-        binding.tvName.text = name
-    }
+        if (!name.isNullOrEmpty()) {
+            mViewModel.getPokemonDetail(name.lowercase(Locale.getDefault()))
+        }
 
-    override fun getViewModel(): PokemonsViewModel = ViewModelProvider(this)[PokemonsViewModel::class.java]
+        lifecycleScope.launchWhenCreated {
+            mViewModel.pokemonDetailStateFlow.collectLatest { details ->
+                binding.tvName.text = details?.name ?: ""
+            }
+        }
+    }
 }
